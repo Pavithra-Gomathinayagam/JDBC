f4dfe3fd92a83e1a8e3271826bbe6c0d80f93e7b
Zilker Technology <you@example.com>
2019-01-11 09:32:28 +0530

    Added Jdbc

---
+++ src/com/zilker/jdbc/JDBC.java
@@ -0,0 +1,36 @@
+package com.zilker.jdbc;
+import java.util.Scanner;
+import java.util.logging.*;
+
+import com.zilker.beans.Student;
+import com.zilker.dao.StudentDAO;
+
+/*
+ * 
+ */
+public class JDBC {
+	
+	private static final Logger logger = Logger.getLogger(JDBC.class.getName());
+	
+	public static void main (String[] args) {
+		Scanner scn = new Scanner(System.in);
+		try {
+			
+			do {
+			StudentDAO studentDAO = new StudentDAO();
+			System.out.print("Enter the regID: ");
+			String regID = scn.next();
+			Student student = new Student(regID, "Raxit", "Jain", "rjain@ztech.io");
+			studentDAO.insertStudent(student);
+			studentDAO.displayStudent(regID);
+			}
+			while(true);
+			
+		}catch (Exception e) {
+		logger.log(Level.SEVERE, e.getMessage());
+		} finally {
+		scn.close();
+		}
+	}
+}
+
